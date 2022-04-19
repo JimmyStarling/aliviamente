9b89b7bf2dcb5f0c745464f5d3730e2fde4d451a
JimmyStarling <JimmyStarlingDev@gmail.com>
2022-04-19 00:55:41 -0300

    Rename Home component to navbar.

---
+++ lib/components/navbar.dart
@@ -0,0 +1,58 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import '../models/constants.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+import './homescreen.dart';
+import './explore.dart';
+import './profile.dart';
+
+class Navbar extends StatefulWidget {
+	@override
+  _NavbarState createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+	int _currentIndex = 0;
+
+	final List<Widget> _screens = [
+		HomeScreen(),	
+		ExploreScreen(),
+		ProfileScreen()
+	];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.grey),
+        home: Scaffold(
+            backgroundColor: Colors.grey.shade100,
+						body: _screens.elementAt(_currentIndex),
+						bottomNavigationBar: BottomNavigationBar(
+							currentIndex: _currentIndex,
+							onTap: onTabTapped,
+							items: [
+								BottomNavigationBarItem(
+									icon: Icon(Icons.home),
+									label:"Home"	
+								),
+								BottomNavigationBarItem(
+									icon: Icon(Icons.search),
+									label: "Explorar"
+								),
+								BottomNavigationBarItem(
+									icon: Icon(Icons.person),
+									label: "Conta"
+								),
+							],
+						),
+					)
+			);
+	}
+	void onTabTapped(int index) {
+		setState(() {
+			_currentIndex = index;
+		});
+	}
+}
